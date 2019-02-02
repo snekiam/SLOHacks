cc78dcde8d6ec435edf30a6c93b27a3a54a9e346
Miles Aikens <snekiam@gmail.com>
2019-02-01 21:14:16 -0800

    Returns a json object to the browser searching for an artist

--- backend/test.py
+++ backend/test.py
@@ -1,10 +1,30 @@
 from flask import Flask
+from flask import jsonify
+
+import configparser
+import spotipy
+import spotipy.oauth2 as oauth2
+import spotipy.util as util
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/login')
+@app.route('/spotify-login')
 def login():
-    return 'login'
+    config = configparser.ConfigParser()
+    config.read('config.cfg')
+    client_id = config.get('SPOTIFY', 'CLIENT_ID')
+    client_secret = config.get('SPOTIFY', 'CLIENT_SECRET')
+    auth = oauth2.SpotifyClientCredentials(
+        client_id=client_id,
+        client_secret=client_secret
+    )
+    token = auth.get_access_token()
+    spotify = spotipy.Spotify(auth=token)
+    results = spotify.search(q='artist:' + "Muse", type='artist')
+    results_json = jsonify(results)
+    print(results_json)
+    return jsonify(results)
+    # return 
