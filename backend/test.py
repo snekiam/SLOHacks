3d30dc190e8fb392d6b092cc1ae381d4a5d3d096
Miles Aikens <snekiam@gmail.com>
2019-02-02 16:17:39 -0800

    Fixed a bug in test.py.  Added 49 more tracks to top_user_tracks

--- backend/test.py
+++ backend/test.py
@@ -41,17 +41,16 @@ def handle_callback():
     token = sp_oauth.get_access_token(code)
     spotify = spotipy.Spotify(auth=token['access_token'])
     # return(jsonify(spotify.search(q='genre:pop', market='US', limit=50)['tracks']['items'][0]['id']))
-    top_songs_audio_features = get_genre_attributes(spotify)
-    spotify.user_playlist_create(sp_oauth)
+    top_songs_audio_features = get_top_song_attributes(spotify)
+    # spotify.user_playlist_create(sp_oauth)
     return top_songs_audio_features
 
 def get_top_song_attributes(spotify):
     audio_features = []
-    # get the user's current top 1,000 songs and their attributes
-    # this will take several seconds to run
-    for i in range(0, 20):
-        track_ids = [song['id'] for song in spotify.current_user_top_tracks(limit=50, offset=50*i + 1)['items']]
-        audio_features.extend(spotify.audio_features(track_ids))
+    # get the user's current top 99 songs (the max spotify will let you)
+    track_ids = [song['id'] for song in spotify.current_user_top_tracks(limit=50, offset=0)['items']]
+    track_ids.extend([song['id'] for song in spotify.current_user_top_tracks(limit=50, offset=49)['items']])
+    audio_features = spotify.audio_features(track_ids)
     return(jsonify(audio_features))
 
 def get_genre_attributes(spotify):
